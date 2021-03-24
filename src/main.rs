b2009b82987cf04fafcf484eb0fc07cd3b7bd8e6
Joeyscat <zhouyu.gg@qq.com>
2021-03-25 01:09:32 +0800

    niubility+1

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
-use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, post, Responder, web};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use serde::Deserialize;
-use serde_json::{Map, Value};
+use serde_json::Value;
 
 #[derive(Deserialize, Debug)]
 struct Request {
@@ -29,7 +29,7 @@ struct Api {
     response: Response,
 }
 
-fn read_api_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Api>, Box<Error>> {
+fn read_api_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Api>, Box<dyn Error>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
@@ -38,18 +38,33 @@ fn read_api_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Api>, Box<Error>> {
     Ok(apis)
 }
 
-#[get("/**")]
-async fn echo(req: HttpRequest) -> impl Responder {
+// #[get("/**")]
+async fn echo(req: HttpRequest) -> HttpResponse {
     let req_key = req.method().to_string() + &":".to_string() + req.uri().path();
 
     let api_arr = read_api_from_file("./test.json").unwrap();
-    for api in &api_arr {
-        let key = api.request.method.to_string() + &":".to_string() + &api.request.uri.to_string();
+    for api in api_arr {
+        let key =
+            api.request.method.to_uppercase() + &":".to_string() + &api.request.uri.to_string();
         if key == req_key {
-            HttpResponse::Ok().body(&api.response.into())
+            return match &api.response.text {
+                Some(text) => HttpResponse::Ok().body(text),
+                None => {
+                    return match api.response.json {
+                        Some(json) => {
+                            let j = serde_json::to_string(&json);
+                            return match j {
+                                Ok(s) => HttpResponse::Ok().body(s.clone()),
+                                Err(e) => HttpResponse::Ok().body(e.to_string()),
+                            };
+                        }
+                        None => HttpResponse::Ok().body(">_<"),
+                    }
+                }
+            };
         }
     }
-    HttpResponse::Ok().body(())
+    HttpResponse::NotFound().body("Not Found")
 }
 
 fn load_map() {
@@ -59,18 +74,19 @@ fn load_map() {
         let key = api.request.method.to_string() + &":".to_string() + &api.request.uri.to_string();
         println!("add {} to map", key);
         api_map.insert(key, api);
-    };
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(echo)
+            // .service(echo)
+            .service(web::resource("/**").route(web::route().to(echo)))
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
 
 #[cfg(test)]
@@ -78,10 +94,9 @@ mod tests {
     use std::error::Error;
     use std::fs::File;
     use std::io::BufReader;
-    use std::path::Path;
 
-    use serde_json::Value;
     use serde_json::value::Value::{Array, Object};
+    use serde_json::Value;
 
     use crate::Api;
 
@@ -100,7 +115,6 @@ mod tests {
         println!("result: {:#?}", result);
     }
 
-
     fn read_json() -> Result<Value, Box<Error>> {
         let file = File::open("test.json")?;
         let reader = BufReader::new(file);
@@ -115,5 +129,3 @@ mod tests {
         println!("result: {:#?}", result);
     }
 }
-
-
