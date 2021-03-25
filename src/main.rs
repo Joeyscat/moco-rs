17a6d191598a2a21feba82d28cf548d3f4250f2f
Joeyscat <zhouyu.gg@qq.com>
2021-03-25 18:33:09 +0800

    加入命令行参数解析: 支持指定服务启动端口

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, web};
+use clap::{App as ClapApp, Arg};
 
 use crate::json_reader::read_api_from_file;
 
@@ -38,12 +39,55 @@ async fn echo(req: HttpRequest) -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let matches = ClapApp::new("moco-rs")
+        .version("0.0.1")
+        .author("ZhouYu <zhouyu.gg@qq.com>")
+        .about("API模拟")
+        .arg(Arg::new("json")
+            .short('j')
+            .long("json")
+            .value_name("JSON")
+            .about("指定加载的API列表文件")
+            .takes_value(true))
+        .arg(Arg::new("port")
+            .short('p')
+            .long("port")
+            .value_name("PORT")
+            .about("指定服务端口号")
+            .takes_value(true))
+        .arg(Arg::new("v")
+            .short('v')
+            .multiple(true)
+            .about("指定日志等级"))
+        .subcommand(ClapApp::new("test")
+            .about("controls testing features")
+            .version("0.0.1")
+            .author("")
+            .arg(Arg::new("debug")
+                .short('d')
+                .about("")))
+        .get_matches();
+
+    let mut json_path = "test.json";
+    let mut port = 8000;
+
+    if let Some(v) = matches.value_of("json") {
+        println!("Value for json: {}", v);
+        json_path = v;
+    }
+    if let Some(v) = matches.value_of("port") {
+        port = v.parse().unwrap();
+    }
+
+
+    println!("App Listening on port: {}", port);
+
     HttpServer::new(|| {
         App::new()
             // .service(echo)
             .service(web::resource("/**").route(web::route().to(echo)))
     })
-        .bind("127.0.0.1:8000")?
+        .bind(format!("127.0.0.1:{}", port))?
         .run()
         .await
 }
